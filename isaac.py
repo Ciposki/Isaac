b5b53bbb647183ecfa5f7d3dda609f633ba33eaf
“GIULIOHHH” <iosonogiulioareste@gmail.com>
2024-03-07 17:16:11 +0100

    HEPL

--- isaac.py
+++ isaac.py
@@ -54,3 +54,51 @@ def release(event):
 window.bind("<KeyPress>",input)
 window.bind("<KeyRelease>",release)
 window.mainloop()
+
+
+
+"""
+import tkinter as tk
+import math
+import random
+window = tk.Tk()
+window.geometry("1920x1080")
+#A dictionary matching keycode with its direction
+orientations={87:(1,-1),83:(1,1),65:(0,-1),68:(0,1)}
+
+class player():
+    def __init__(self):
+        self.direction = [0,0]
+        self.pos = [0,0]
+        self.speed = 3
+        self.init_draw()
+    def init_draw(self):
+        self.player_geometry = tk.Canvas(window,bg="blue",height=100,width=100)
+        self.player_geometry.place(x=self.pos[0],y=self.pos[1])
+    def update_state(self):
+        #If going diagonally
+        if self.direction[0]&self.direction[1]:
+            print(self.direction)
+            self.pos = [self.pos[0]+self.direction[0]*self.speed*math.sqrt(2)/2,self.pos[1]+self.direction[1]*self.speed*math.sqrt(2)/2]
+        else:
+            self.pos = [self.pos[0]+self.direction[0]*self.speed,self.pos[1]+self.direction[1]*self.speed]
+        self.player_geometry.place(x=self.pos[0],y=self.pos[1])
+        
+
+Giova = player()
+
+def input(event):
+    if event.keycode in orientations:
+        rotate=orientations[event.keycode]
+        Giova.direction[rotate[0]]=rotate[1]
+        Giova.update_state()
+        
+def release(event):
+    if event.keycode in orientations:
+        rotate=orientations[event.keycode]
+        Giova.direction[rotate[0]]=0
+
+window.bind("<KeyPress>",input)
+window.bind("<KeyRelease>",release)
+window.mainloop()
+"""
